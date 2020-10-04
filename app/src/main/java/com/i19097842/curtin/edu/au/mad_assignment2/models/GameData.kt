31e5b51d1593410e437c89e41c137e66541bc1d2
Seain Malkin <seainmalkin@gmail.com>
2020-10-05 03:56:10 +0800

    Removed unused constants

--- app/src/main/java/com/i19097842/curtin/edu/au/mad_assignment2/models/GameData.kt
+++ app/src/main/java/com/i19097842/curtin/edu/au/mad_assignment2/models/GameData.kt
@@ -2,11 +2,6 @@ package com.i19097842.curtin.edu.au.mad_assignment2.models
 
 import com.i19097842.curtin.edu.au.mad_assignment2.lib.MapData
 
-//TODO: Replace with settings object
-const val HEIGHT = 10
-const val WIDTH = 30
-
-
 /**
  * Represents the current game been played. Implemented as a Singleton as only one game can be
  * loaded at a time. Data is read from the database on the first access and cached in memory. If
