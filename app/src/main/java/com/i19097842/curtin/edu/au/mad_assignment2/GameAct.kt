b06532a85d372ff0d6920e374461ebc364079b39
Seain Malkin <seainmalkin@gmail.com>
2020-10-06 00:17:52 +0800

    Fragment loading and switching improved

--- app/src/main/java/com/i19097842/curtin/edu/au/mad_assignment2/GameAct.kt
+++ app/src/main/java/com/i19097842/curtin/edu/au/mad_assignment2/GameAct.kt
@@ -4,9 +4,19 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.i19097842.curtin.edu.au.mad_assignment2.fragments.MapFrag
-import com.i19097842.curtin.edu.au.mad_assignment2.fragments.MetaFrag
 import com.i19097842.curtin.edu.au.mad_assignment2.fragments.StructListFrag
+import com.i19097842.curtin.edu.au.mad_assignment2.fragments.StructSelectFrag
+
+/** Arguments for state bundles */
+private const val ARG_STRUCT_MODE
+        = "com.i19097842.curtin.edu.au.mad_assignment2.game_act.arg_struct_mode"
+
+/** Constants for determining which fragment is loaded as the structure fragment */
+private const val STRUCT_FRAG_SELECT = "struct_frag_select"
+private const val STRUCT_FRAG_LIST = "struct_frag_list"
+
 
 /**
  * GameData.kt Activity: This is the games core activity. Content is displayed across four fragments
@@ -18,23 +28,14 @@ import com.i19097842.curtin.edu.au.mad_assignment2.fragments.StructListFrag
  * @author Seain Malkin (19097842@student.curtin.edu.au)
  */
 class GameAct : MapFrag.MapListener, AppCompatActivity() {
-    /**
-     * Injects the fragments into the main view and reinstates any saved state data
-     * @see [AppCompatActivity.onCreate]
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+    /** @property[structFragMode] The current mode of the structure fragment */
+    private var structFragMode = STRUCT_FRAG_SELECT
 
-        // Load fragments using the fragment manager
-        supportFragmentManager.let { fm ->
-            // Begin a transaction and add fragments if needed
-            if (fm.findFragmentById(R.id.gameFrameStructList) !is StructListFrag) {
-                fm.beginTransaction()
-                    .add(R.id.gameFrameStructList, StructListFrag.newInstance()).commit()
-            }
-        }
-    }
+    /** @property[structListFrag] Instance of the structure list fragment */
+    private var structListFrag: StructListFrag? = null
+
+    /** @property[structSelectFrag] Instance of the structure select fragment */
+    private var structSelectFrag: StructSelectFrag? = null
 
     companion object {
         /**
@@ -45,6 +46,97 @@ class GameAct : MapFrag.MapListener, AppCompatActivity() {
         @JvmStatic
         fun getIntent(context: Context): Intent {
             return Intent(context, GameAct::class.java)
+        }
+    }
+
+    /**
+     * Injects the fragments into the main view and reinstates any saved state data
+     * @see [AppCompatActivity.onCreate]
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        // Resume any previous state
+        savedInstanceState?.let {
+            structFragMode = savedInstanceState.getString(ARG_STRUCT_MODE, STRUCT_FRAG_SELECT)
+        }
+
+        // Decide which structure fragment to display
+        when (structFragMode == STRUCT_FRAG_SELECT) {
+            true -> loadStructFragSelect()
+            false -> loadStructFragList()
+        }
+    }
+
+    /**
+     * Save activity state
+     * @see [AppCompatActivity.onSaveInstanceState]
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(ARG_STRUCT_MODE, structFragMode)
+        super.onSaveInstanceState(outState)
+    }
+
+    /**
+     * Loads the structure select fragment
+     */
+    private fun loadStructFragSelect() {
+        // Create an instance if needed
+        if (structSelectFrag == null) {
+            supportFragmentManager.let { fm ->
+                fm.findFragmentById(R.id.gameFrameStruct).let {
+                    // Either reference the found fragment or create a new instance
+                    structSelectFrag = when (it) {
+                        is StructSelectFrag -> it
+                        else -> StructSelectFrag.newInstance()
+                    }
+                }
+            }
+        }
+        // Display fragment and hide its counterpart
+        toggleStructFrag(structSelectFrag, structListFrag)
+    }
+
+    /**
+     * Loads the structure list fragment
+     * Exactly the same logic as previous function but a generic version won't work as you
+     * can't call a static method from a generic class (as far as I could tell)
+     */
+    private fun loadStructFragList() {
+        // Create an instance if needed
+        if (structListFrag == null) {
+            supportFragmentManager.let { fm ->
+                fm.findFragmentById(R.id.gameFrameStruct).let {
+                    // Either reference the found fragment or create a new instance
+                    structListFrag = when (it) {
+                        is StructListFrag -> it
+                        else -> StructListFrag.newInstance()
+                    }
+                }
+            }
+        }
+        // Display fragment and hide its counterpart
+        toggleStructFrag(structListFrag, structSelectFrag)
+    }
+
+    /**
+     * Displays one fragment and hides the other
+     * @param[on] The fragment to display
+     * @param[off] The fragment to hide
+     */
+    private fun toggleStructFrag(on: Fragment?, off: Fragment?) {
+        supportFragmentManager.let { fm ->
+            fm.beginTransaction().run {
+                on?.let { frag ->
+                    when (frag.isAdded) {
+                        true -> show(frag)
+                        false -> add(R.id.gameFrameStruct, frag)
+                    }
+                    off?.let { hide(it) }
+                    commit()
+                }
+            }
         }
     }
 }
